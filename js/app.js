0c1ece22ae30484793bbb4bf38a32bb1e0bb4148
Richard Strutt <richardastrutt@gmail.com>
2016-04-12 13:15:04 +0800

    added model data, clickable cats, used var self = this trick, finalized project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
-var Cat = function(){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.nickNames = ko.observableArray(['Tabbitha', 'Tabs', 'Tabbarina']);
+var initalCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		nickNames: ['Tabbitha', 'Tabs', 'Tabbarina']
+	},{
+		clickCount: 0,
+		name: 'Bob',
+		imgSrc: 'img/1413379559_412a540d29_z.jpg',
+		nickNames: ['Bobbie', 'Bobs', 'BB']		
+	},{
+		clickCount: 0,
+		name: 'Alex',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		nickNames: ['Ali', 'Al', 'Aldo']
+	},{
+		clickCount: 0,
+		name: 'Sheela',
+		imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+		nickNames: ['She', 'Lea', 'Sheelapoop']
+	}
+];
+
+var Cat = function(data){
+
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nickNames = ko.observableArray(data.nickNames);
 	this.catLevel = ko.computed(function(){
 		var catLevel;
 		var clicks = this.clickCount();
@@ -21,11 +46,23 @@ var Cat = function(){
 }
 
 var ViewModel = function() {
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initalCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+	});
+
 	//store our cat a new current cat variable
-	this.currentCat = ko.observable( new Cat() );
+	this.currentCat = ko.observable( this.catList()[0] );
 	//clicking on cat increments the clickCounter
 	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.clickCat = function(clickedCat){
+		self.currentCat(clickedCat);
 	};
 }
 
