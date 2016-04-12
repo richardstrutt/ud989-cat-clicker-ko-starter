fb62197aefad0a9ba5976766d313aa8105fde74a
Richard Strutt <richardastrutt@gmail.com>
2016-04-12 11:38:08 +0800

    moved all cat model into the new Cat function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
-var ViewModel = function() {
+var Cat = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.nickNames = ko.observableArray(['Tabbitha', 'Tabs', 'Tabbarina']);
-
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
-	};
-
 	this.catLevel = ko.computed(function(){
 		var catLevel;
 		var clicks = this.clickCount();
@@ -23,7 +18,15 @@ var ViewModel = function() {
 		}
 		return catLevel;
 	}, this);
+}
 
+var ViewModel = function() {
+	//store our cat a new current cat variable
+	this.currentCat = ko.observable( new Cat() );
+	//clicking on cat increments the clickCounter
+	this.incrementCounter = function(){
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
 }
 
 ko.applyBindings(new ViewModel());
