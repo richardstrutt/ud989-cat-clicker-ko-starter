5327dd10cb90a1e9e3439b51a76e580c70b2668e
Richard Strutt <richardastrutt@gmail.com>
2016-04-12 11:29:20 +0800

    added nicknames using control flow KO

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.nickNames = ko.observableArray(['Tabbitha', 'Tabs', 'Tabbarina']);
 
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() + 1);
